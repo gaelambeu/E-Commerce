0050c4f84c0fea3c8f92cd1018841fe4b9fb4aff
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-03 17:05:30 +0000

    create navigation

--- Bedroom/navigation/BottomTabNavigation.jsx
+++ Bedroom/navigation/BottomTabNavigation.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigation } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigation();
 
 const BottomTabNavigation = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Tab.Navigation>
+            <Tab.Screens name ="Home" components={Home}/>
+        </Tab.Navigation>
     )
 }
 
