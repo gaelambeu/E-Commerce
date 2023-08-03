59984af5959e7a0accbdb1a8275f0bf1b7688c82
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-03 16:03:18 +0000

    import font

--- Bedroom/App.js
+++ Bedroom/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFont } from 'expo-font';
+import { useFont, useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  })
+
   return (
     <View style={styles.container}>
       <Text> to start working on your app!</Text>
