7b088a9ccd80a937a6ab326ca23a61a2deec36f3
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-04 22:09:37 +0000

    create orthers pages

--- Bedroom/navigation/BottomTabNavigation.jsx
+++ Bedroom/navigation/BottomTabNavigation.jsx
@@ -1,14 +1,39 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigation } from '@react-navigation/bottom-tabs';
+import { Ionicon } from "@expo/vector-icons"
 
 const Tab = createBottomTabNavigation();
 
+const screenOptions = {
+    tabBarShowLabel: false,
+    tabBarHideOnKeyboard: true,
+    headerShown: false,
+    tabBarStyle: {
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        height: 70
+    }
+}
+
 const BottomTabNavigation = () => {
     return (
-        <Tab.Navigation>
-            <Tab.Screens name ="Home" components={Home}/>
-        </Tab.Navigation>
+        <Tab.Navigation screenOptions={screenOptions}>
+            <Tab.Screens 
+                name ="Home" 
+                components={Home}
+                options={{
+                    tabBarIcon: ({focused}) => {
+                        return <></>
+                    }
+                }}
+                />
+            <Tab.Screens name ="Search" components={Search}/>
+            <Tab.Screens name ="Profile" components={Profile}/>
+        </Tab.Navigation> 
     )
 }
 
---
+++ Bedroom/screens/Search.jsx
@@ -0,0 +1,14 @@
+import {  StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+
+
+
+const Search = () => {
+    return (
+        <Text>Search</Text>
+    )
+}
+
+export default Search
+
+const styles = StyleSheet.create({}) 
