8dc902ba8c5e9e1c3bd4bcc732596da791df508e
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-08 16:26:40 +0000

    short

--- Bedroom/App.js
+++ Bedroom/App.js
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
