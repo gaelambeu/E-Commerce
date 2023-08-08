59713286cd4f473a3447adbb6f1d0e0aabe59c17
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-08 22:10:11 +0000

    App.js

--- Macase/App.js
+++ Macase/App.js
@@ -1,7 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'regular': require('./assets/fonts/Poppins-Regular'),
+    'light': require('./assets/fonts/Poppins-Light'),
+    'bold': require('./assets/fonts/Poppins-Bold'),
+    'medium': require('./assets/fonts/Poppins-Medium'),
+    'extrabold': require('./assets/fonts/Poppins-ExtraBold'),
+    'semibold': require('./assets/fonts/Poppins-SemiBold'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -17,4 +41,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily: "extrabold",
+    fontSize: 20,
+  }
 });
