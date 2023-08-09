bd012674e1d9c3abbe9a46cd9d711957ff9c5fcd
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-09 00:37:21 +0000

    tout est ok

--- Macase/App.js
+++ Macase/App.js
@@ -1,8 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,10 +31,15 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Bottom Navigation" 
+          component={BottomTabNavigation} 
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
