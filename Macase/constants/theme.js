0d13afebde46eb7ede62c2aa90aa132e45aafbf7
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-10 23:05:26 +0000

    color
    is correct

--- Macase/constants/theme.js
+++ Macase/constants/theme.js
@@ -3,7 +3,7 @@ const { height, width } = Dimensions.get('window');
 
 const  COLORS = {
     primary: "#2A4058",
-    secondary: "#DDFB0FF",
+    secondary: "#DDf0FF",
     tertiary: "#FF7754",
 
     gray: "#83829A",
