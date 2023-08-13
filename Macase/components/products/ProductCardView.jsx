f8831672d8b07cf294781a925cb0b34ed421673f
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-13 20:24:53 +0000

    make ProductCardView

---
+++ Macase/components/products/ProductCardView.jsx
@@ -0,0 +1,15 @@
+import { Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const ProductCardView = () => {
+  return (
+    <TouchableOpacity onPress={() => {}}>
+        <View style={styles.container}> 
+
+        </View>
+    </TouchableOpacity>
+  )
+}
+
+export default ProductCardView
+
---
+++ Macase/components/products/ProductCardView.style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../constants/index";
+
+const styles = StyleSheet.create({
+    container:{
+        width: 182,
+        height: 240,
+        marginEnd: 22,
+    }
+})
+
+export default styles
