3fb7b7e79f5fddb1ac3c4b3fd6eb1d475efc9041
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-13 19:51:09 +0000

    make headings

--- Macase/components/home/headings.style.js
+++ Macase/components/home/headings.style.js
@@ -2,7 +2,15 @@ import { StyleSheet } from "react-native";
 import { COLORS, SIZES } from "../../constants/index";
 
 const styles = StyleSheet.create({
-    
+    container:{
+        marginTop: SIZES.medium,
+        marginBottom: -SIZES.xSmall,
+        marginHorizontal: 12
+    },
+    header:{
+        flexDirection:"row",
+        
+    }
 })
 
 export default styles
--- Macase/screens/Home.jsx
+++ Macase/screens/Home.jsx
@@ -5,6 +5,7 @@ import { Ionicons, Fontisto } from '@expo/vector-icons';
 import styles from './home.style'
 import { Welcome } from '../components';
 import Carousel from '../components/home/Carousel';
+import Headings from '../components/home/Headings';
 
 
 const Home = () => {
@@ -33,6 +34,7 @@ const Home = () => {
         <ScrollView>
           <Welcome />
           <Carousel />
+          <Headings />
         </ScrollView>
     </SafeAreaView>
   )
