c4e4d19658ffa348e9d038815f8a2078f5f1a831
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-13 20:09:11 +0000

    make ProductRow

---
+++ Macase/components/products/ProductRow.jsx
@@ -0,0 +1,15 @@
+import { FlatList, Text, View } from 'react-native'
+import React from 'react'
+import { SIZES } from '../../constants'
+
+const ProductRow = () => {
+    const products = [1, 2, 3, 4]
+
+  return (
+    
+    
+  )
+}
+
+export default ProductRow
+
