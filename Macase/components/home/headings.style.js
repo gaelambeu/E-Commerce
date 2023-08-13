194f1db0186332d3a21e6a3995caa3c25f754b21
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-13 19:55:04 +0000

    finish headings

--- Macase/components/home/headings.style.js
+++ Macase/components/home/headings.style.js
@@ -4,12 +4,16 @@ import { COLORS, SIZES } from "../../constants/index";
 const styles = StyleSheet.create({
     container:{
         marginTop: SIZES.medium,
-        marginBottom: -SIZES.xSmall,
+        //marginBottom: -SIZES.xSmall,
         marginHorizontal: 12
     },
     header:{
         flexDirection:"row",
-        
+        justifyContent: "space-between"
+    }, 
+    headerTitle: {
+        fontFamily: "semibold",
+        fontSize: SIZES.xLarge -2,
     }
 })
 
