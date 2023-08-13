b8842ab1705bdce6830bf12b160a5ee349205749
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-13 20:32:35 +0000

    productcardstyle

--- Macase/components/products/ProductCardView.jsx
+++ Macase/components/products/ProductCardView.jsx
@@ -1,11 +1,15 @@
 import { Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import styles from './ProductCardView.style'
+
 
 const ProductCardView = () => {
   return (
     <TouchableOpacity onPress={() => {}}>
         <View style={styles.container}> 
-
+            <View style={styles.imageContainer}>
+                
+            </View>
         </View>
     </TouchableOpacity>
   )
--- Macase/components/products/ProductCardView.style.js
+++ Macase/components/products/ProductCardView.style.js
@@ -6,6 +6,16 @@ const styles = StyleSheet.create({
         width: 182,
         height: 240,
         marginEnd: 22,
+        borderRadius: SIZES.medium,
+        backgroundColor: COLORS.secondary
+    }, 
+    imageContainer: {
+        flex: 1,
+        width: 170,
+        marginLeft: SIZES.small/2,
+        marginTop: SIZES.small/2,
+        borderRadius: SIZES.small,
+        overflow: "hidden",
     }
 })
 
