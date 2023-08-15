2feac57a02032d72584a6f02b04c7e73e20c39f5
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-15 17:28:52 +0000

    make productDetails

--- Macase/navigation/BottomTabNavigation.jsx
+++ Macase/navigation/BottomTabNavigation.jsx
@@ -18,7 +18,7 @@ const screenOptions = {
         right: 0,
         left: 0,
         elevation: 0,
-        height: 70
+        height: 50
     }
 
 }
--- Macase/screens/ProductDetails.jsx
+++ Macase/screens/ProductDetails.jsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import styles from './productDetails.style'
+import { Ionicons } from '@expo/vector-icons'
+import {COLORS, SIZES} from '../constants/index' 
 
-const ProductDetails = () => {
+const ProductDetails = ({navigation}) => {
   return (
-    <View>
-      <Text>ProductDetails</Text>
+    <View style={styles.container}>
+      <View style={styles.upperRow}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="chevron-back-circle" size={30} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}}>
+          <Ionicons name="heart" size={30} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+      <Image 
+      source={{uri: "https://i.pinimg.com/564x/88/b9/82/88b9820cb6ddbeaf55ff56c08033cc8f.jpg"}}
+      />
+
     </View>
   )
 }
 
 export default ProductDetails
-
-const styles = StyleSheet.create({})
