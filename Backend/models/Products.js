889ffec03ff6e5fe15447681ece8a954d7003511
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-18 23:07:05 +0000

    put it

--- Backend/models/Products.js
+++ Backend/models/Products.js
@@ -6,6 +6,6 @@ const ProductSchema = new mongoose.Schema({
     imageUrl: {type: String, require: true},
     description: {type: String, require: true},
     product_location: {type: String, require: true},
-}, );
+}, {timestamps: true});
 
 module.export = mongoose.model("Product", ProductSchema)
