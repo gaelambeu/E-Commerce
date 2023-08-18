ae98ad4a6fc13d72fadff4dee60766a3f518cda8
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-18 00:34:55 +0000

    short modif

--- Backend/index.js
+++ Backend/index.js
@@ -8,5 +8,11 @@ const port = 3000
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connect")).catch((err) => console.log(err))
 
-app.get('/', (req, res) => res.send('Furnirture World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb', extended: true}))
+
+
+app.use('/api/products', productRouter)
+
+
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
