27d127f6af73c6fbd1150c9eaeeb20743340c081
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-18 00:02:20 +0000

    productsControllers.js
    three party end

--- Backend/controllers/productsControllers.js
+++ Backend/controllers/productsControllers.js
@@ -28,4 +28,27 @@ module.exports = {
             res.status(500).json("failed to get the product")
         }
     },
+
+    searchProduct: async(req, res) => {
+        try {
+            const result = await Product.aggregate(
+                [
+                    {
+                      $search: {
+                        index: "furniture",
+                        text: {
+                          query: req.params.key,
+                          path: {
+                            wildcard: "*"
+                          }
+                        }
+                      }
+                    }
+                ]
+            )
+            res.status(200).json(result)
+        } catch (error) {
+            res.status(500).json("failed to get the products")
+        }
+    }
 }
