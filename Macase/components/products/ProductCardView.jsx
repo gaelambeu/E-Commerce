d62a8ee819a1254854d8a8d2eeefcc151db3e3f6
GAEL AMBEU <ambeugael128@gmail.com>
2023-08-21 23:38:18 +0000

    short modif

--- Macase/components/products/ProductCardView.jsx
+++ Macase/components/products/ProductCardView.jsx
@@ -6,7 +6,7 @@ import { COLORS } from '../../constants'
 import { useNavigation } from '@react-navigation/native'
 
 
-const ProductCardView = ({item}) => {
+const ProductCardView = ({}) => {
     const navigation = useNavigation();
 
   return (
